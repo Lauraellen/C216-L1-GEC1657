064c25cb1df60f8f6bbf9308cdf8d10409fb54df
laura.souza <laura.souza@gclaims.com.br>
2024-03-03 17:28:54 -0300

    pratica-0

---
+++ pratica-0/src/index.js
@@ -0,0 +1,8 @@
+let nome = "Laura Ellen - 1657";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mensagem: 'Testando modulo: Laura Ellen - 1657 !',
+    saudacao: function () {
+        console.log(this.mensagem);
+    }
+};
